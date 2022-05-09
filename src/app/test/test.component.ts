7ac9dbf5eb76ddb5f28c95499fd5c27ef111e7e3
Harry <hdhillon478@gmail.com>
2022-05-09 10:17:45 +0100

    Add basic string interpolation

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  styleUrls: ['./test.component.css'],
 })
 export class TestComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  randomComment = 'OMG Harry, we are finally learning Angular';
 }

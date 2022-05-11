63f6e691bd1d700efab613ec87d62639437fd31f
Harry <hdhillon478@gmail.com>
2022-05-11 08:15:03 +0100

    Create directives component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { ServersComponent } from './servers/servers.component';
+import { DirectivesComponent } from './directives/directives.component';
 
 @NgModule({
-  declarations: [AppComponent, TestComponent, ServersComponent],
+  declarations: [AppComponent, TestComponent, ServersComponent, DirectivesComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],

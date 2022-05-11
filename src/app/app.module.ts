46382384bd270146e43a9aef1022d04d004ba112
Harry <hdhillon478@gmail.com>
2022-05-11 08:23:30 +0100

    Remove test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
 import { ServersComponent } from './servers/servers.component';
 import { DirectivesComponent } from './directives/directives.component';
 
 @NgModule({
-  declarations: [AppComponent, TestComponent, ServersComponent, DirectivesComponent],
+  declarations: [AppComponent, ServersComponent, DirectivesComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],

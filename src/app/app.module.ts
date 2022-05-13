86a498f5f04fb1bb155d8fff66777c6b6018008b
Harry <hdhillon478@gmail.com>
2022-05-13 08:58:49 +0100

    Create new form-element component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { ServersComponent } from './servers/servers.component';
 import { DirectivesComponent } from './directives/directives.component';
 import { ComponentsAndDatabindingComponent } from './components-and-databinding/components-and-databinding.component';
+import { FormElementComponent } from './components-and-databinding/form-element/form-element.component';
+import { ServerElementComponent } from './components-and-databinding/server-element/server-element.component';
 
 @NgModule({
-  declarations: [AppComponent, ServersComponent, DirectivesComponent, ComponentsAndDatabindingComponent],
+  declarations: [AppComponent, ServersComponent, DirectivesComponent, ComponentsAndDatabindingComponent, FormElementComponent, ServerElementComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],

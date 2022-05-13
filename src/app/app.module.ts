bca43377b7e823aa44ffce259dd80167ef2e1e9a
Harry <hdhillon478@gmail.com>
2022-05-13 08:57:09 +0100

    Generate new components-and-databinding component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServersComponent } from './servers/servers.component';
 import { DirectivesComponent } from './directives/directives.component';
+import { ComponentsAndDatabindingComponent } from './components-and-databinding/components-and-databinding.component';
 
 @NgModule({
-  declarations: [AppComponent, ServersComponent, DirectivesComponent],
+  declarations: [AppComponent, ServersComponent, DirectivesComponent, ComponentsAndDatabindingComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components-and-databinding/components-and-databinding.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-components-and-databinding',
+  templateUrl: './components-and-databinding.component.html',
+  styleUrls: ['./components-and-databinding.component.css']
+})
+export class ComponentsAndDatabindingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

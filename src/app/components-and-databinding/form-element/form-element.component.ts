0fe881442478395db607d93fba4d6725366d3b16
Harry <hdhillon478@gmail.com>
2022-05-13 13:10:44 +0100

    Create custom event emmiters

--- src/app/components-and-databinding/form-element/form-element.component.ts
+++ src/app/components-and-databinding/form-element/form-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-form-element',
@@ -9,6 +9,15 @@ export class FormElementComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  serverCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
+  blueprintCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
 
   newServerName = '';
   newServerContent = '';

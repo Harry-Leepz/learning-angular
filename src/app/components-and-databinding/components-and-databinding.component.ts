67eb11a70ca282b0899792a7141ce765ff9efaf5
Harry <hdhillon478@gmail.com>
2022-05-13 11:59:28 +0100

    Implement custom property binding for element

--- src/app/components-and-databinding/components-and-databinding.component.ts
+++ src/app/components-and-databinding/components-and-databinding.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-components-and-databinding',
   templateUrl: './components-and-databinding.component.html',
-  styleUrls: ['./components-and-databinding.component.css']
+  styleUrls: ['./components-and-databinding.component.css'],
 })
 export class ComponentsAndDatabindingComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  serverElements = [
+    {
+      type: 'server',
+      name: 'Test Server',
+      content: 'This is a test server',
+    },
+  ];
 }
